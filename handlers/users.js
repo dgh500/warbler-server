15d8111d4c01d39b126adfd7069cec5aac99180f
dgh500 <dgh500@gmail.com>
2019-12-16 21:03:45 +0000

    can finally update a user, needed a mongoose option flag to return the new doucument, was having a weird lag effect on state each time

--- handlers/users.js
+++ handlers/users.js
@@ -6,10 +6,11 @@ exports.getUser = () => null;
 exports.updateUser = async function(req, res, next) {
   try {
     let inputUser = {...req.body};
-    let updatedUser = await db.User.findByIdAndUpdate(req.params.id,inputUser)
-    .then((updatedUser) => {
+    // The { new: true } option tells mongoose to return the new updated document - took a lot of debugging to realise this one...
+    let updatedUser = await db.User.findByIdAndUpdate(req.params.id,inputUser,{new:true});
+    // .then((updatedUser) => {
       return res.status(200).json(updatedUser);
-    });
+    // });
   } catch(e) {
     return next(e);
   }
