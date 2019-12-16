20f0acf999050b432ef0883f5eba36ddcada019c
dgh500 <dgh500@gmail.com>
2019-12-16 21:27:06 +0000

    can now fully edit a user including changing picture

--- handlers/users.js
+++ handlers/users.js
@@ -5,7 +5,13 @@ exports.getUser = () => null;
 
 exports.updateUser = async function(req, res, next) {
   try {
-    let inputUser = {...req.body};
+    let inputUser = {};
+    if(req.file !== undefined) {
+      let profileImageUrl = req.file.filename;
+      inputUser = {...req.body, profileImageUrl};
+    } else {
+      inputUser = {...req.body};
+    }
     // The { new: true } option tells mongoose to return the new updated document - took a lot of debugging to realise this one...
     let updatedUser = await db.User.findByIdAndUpdate(req.params.id,inputUser,{new:true});
     // .then((updatedUser) => {
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-
 const { createUser, getUser, updateUser, deleteUser } = require('../handlers/users');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/images');
+  },
+  filename: (req, file, cb) => {
+    let fileType = '';
+    switch (file.mimetype) {
+      case 'image/gif':
+        fileType = 'gif';
+      case 'image/png':
+        fileType = 'png';
+      case 'image/jpeg':
+        fileType = 'jpg';
+    }
+    cb(null, `image-${Date.now()}.${fileType}`);
+  }
+});
+const upload = multer({storage});
 
 // Prefix - /api/users/:id
 router.route('/:id')
   .get(getUser)
-  .put(updateUser)
+  .put(upload.single('profileImageUrl'),updateUser)
   .delete(deleteUser);
 
 module.exports = router;
