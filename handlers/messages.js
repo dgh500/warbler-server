62dd85413dde4766ff51665d87ff892ab9bd2333
dgh500 <dgh500@gmail.com>
2020-01-10 20:25:27 +0000

    Now user stats (for new users at least) are correct and go down when deleted

--- handlers/messages.js
+++ handlers/messages.js
@@ -90,13 +90,14 @@ exports.updateMessage = async function(req, res, next) {
 exports.deleteMessage = async function(req, res, next) {
   try {
     // Also need to check if it has replies, if so these need to be removed before the parent message is removed
-    /*** not actually removing the messages...`` ***/
     let replies = await db.Message.find({_id:req.params.message_id},{replies:1});
     if(replies[0] !== undefined) {
       replies[0].replies.forEach(async function(v,i,a) {
-        await db.Message.findByIdAndRemove(v,function() {
+        let replyMsg = await db.Message.findById(v);
+        await replyMsg.remove();
+        /*await db.Message.findByIdAndRemove(v,function() {
           console.log(v + 'Removed');
-        });
+        });*/
       });
     }
 
